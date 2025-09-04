b39e276c8cb1f05a1a3e0a9cad330b074300dae9
JavierDevPro <gjangsam@gmail.com>
2025-09-04 13:50:06 -0500

    Se hace logica inicial para un cajero (simulado).

--- Program.cs
+++ Program.cs
@@ -1,19 +1,63 @@
-using System;
+﻿using System;
+using ServiciosCajero.service.src;
 
 class Program
 {
-    static void Main()
+    public static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola Mundo!");
-        Console.ReadKey();
+        double saldo = 1000000;
+        string ingreso = null;
+        int eleccion;
+
+        //Pantalla del cajero:
+        Console.WriteLine("\n--- Menu Cajero ---\n");
+        Console.WriteLine("1. Consultar saldo \n2. Depositar dinero \n3. Retirar dinero \n4. Salir \n");
+
+        Console.Write("Que deseas hacer? \n");
+        ingreso = Console.ReadLine();
+
+        while (!int.TryParse(ingreso, out eleccion))
+        {
+            //Pantalla del cajero:
+            Console.WriteLine("\n--- Menu Cajero ---\n");
+            Console.WriteLine("1. Consultar saldo \n2. Depositar dinero \n3. Retirar dinero \n4. Salir \n");
+            Console.WriteLine($"ERROR: '{ingreso}' no es un valor numerico valido.");
+            Console.Write("Que deseas hacer? \n");
+            ingreso = Console.ReadLine();
+        }
+        /***
+        con la condicion en la linea 16 me ahorro lo siguiente:
+        try
+        {
+            eleccion = int.Parse(ingreso);
+        }
+        catch
+        {
+            Console.WriteLine($"ERROR: error al intentar parsear la eleccion ingresada ({ingreso}).");
+        }
+        ***/
+
+        switch (eleccion)
+        {
+            case 1:
+                consultarSaldo(saldo);
+                break;
+            case 2:
+                saldo = depositarSaldo(saldo);
+                break;
+
+        }
+    }
+    static void consultarSaldo(double value)
+    {
+        Console.Write($"Su saldo actual es de: \n{value}$$\n");
+    }
+    static double depositarSaldo(double actualValue)
+    {
+        //Pantalla del cajero:
+        Console.WriteLine("\n--- C ---\n");
+        Console.WriteLine("1. Consultar saldo \n2. Depositar dinero \n3. Retirar dinero \n4. Salir \n");
+        double newValue = 1;
+        return newValue;
     }
 }
-// sudo apt update
-// sudo apt install -y wget
-// wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-// sudo dpkg -i packages-microsoft-prod.deb
-// rm packages-microsoft-prod.deb
-// sudo apt update
-// sudo apt install -y dotnet-sdk-8.0
-
-// dotnet run
